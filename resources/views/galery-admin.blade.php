996d9b741b7fa3a76bbb3879bd2e210e161fd6ef
Herdy Juniawan <herdy478@gmail.com>
2025-01-08 13:52:18 +0800

    first commit

--- resources/views/galery-admin.blade.php
+++ resources/views/galery-admin.blade.php
@@ -51,6 +51,8 @@ $imageDataJson = json_encode($imageData);
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy"
+    content= "upgrade-insecure-requests">
     <title>Galeri Budaya Bali Admin</title>
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
--- resources/views/galery.blade.php
+++ resources/views/galery.blade.php
@@ -53,6 +53,8 @@ $imageDataJson = json_encode($imageData);
 <head>
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta http-equiv="Content-Security-Policy"
+    content= "upgrade-insecure-requests">
     <title>Galeri Budaya Bali</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
